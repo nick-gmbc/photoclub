9e8303e159247df6dae500b8eed073ab7ae11dcc
nick.gnmc <nick.sutton@gbmc.ac.uk>
2025-07-20 12:57:25 +0100

    login fixed

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $servername = "localhost";
+    $db_username = "photo_club_admin";
+    $db_password = "wotApic5%";
+    $db = "photoclub";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $db_username, $db_password, $db);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // matching password and user name
+
+    $query = "SELECT * FROM members WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($conn, $query);
+
+    if ( mysqli_num_rows( $result ) == 1) {
+        $message = "OK";
+    }
+    else {
+        $message = "FAIL";
+    }
+    mysqli_close($conn);
+    echo $message;
+
+?>
